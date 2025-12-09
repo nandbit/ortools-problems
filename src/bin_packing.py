777c82d5a0fe0e12a90194b4528001bdc37ca699
nandbit <nikitagalayda@gmail.com>
2025-12-09 10:34:11 +0800

    finish bin packing

--- src/bin_packing.py
+++ src/bin_packing.py
@@ -15,7 +15,58 @@
 
 from ortools.sat.python import cp_model
 
+from utils import solution_status
+
 items = [4, 8, 1, 4, 2, 1]
 max_capacity = 10
 
+model = cp_model.CpModel()
 
+# Initial number of bins is len(items)
+
+bin_used = []
+for i in range(len(items)):
+    bin_used.append(model.new_bool_var(f"bin_{i}_used"))
+
+# Boolean variables for items being in bins
+# is in that bin
+item_in_bin = {} # bin : item
+for i in range(len(items)): # for each bin
+    for j in range(len(items)): # for each item
+        item_in_bin[i, j] = model.new_bool_var(f"item_{j}_in_bin_{i}")
+
+# Ensure that each item is only in one bin
+for i in range(len(items)): # for each item
+    total = 0
+    for j in range(len(items)): # for each bin
+        total += item_in_bin[j, i]
+    model.add(total == 1)
+
+# Ensure that capacity of each bin doesn't exceed max
+for i in range(len(items)): # for each bin
+    load = 0
+    for j in range(len(items)): # for each item
+      # load = load + item_weight * (0 or 1)
+      # so if item is in this bin, add its weight to the load  
+        load = load + items[j] * item_in_bin[i, j]
+    model.add(load <= max_capacity)
+
+# Make bin used to TRUE if there is an item in it
+for i in range(len(items)): # for each bin
+    for j in range(len(items)): # for each item
+        # if item_in_bin is 1 then bin used will be <= 1
+        # if item_in_bin is 0 then bin used will be <= 0
+        model.add(item_in_bin[i, j] <= bin_used[i])
+
+model.minimize(sum(bin_used))
+solver = cp_model.CpSolver()
+
+status = solution_status(solver.solve(model))
+print(status)
+
+
+print(f"Bins used: {solver.value(sum(bin_used))}")
+for k, v in item_in_bin.items():
+    if (solver.value(v) == 1):
+        print(f"Item {k[1]} in bin {k[0]}")
+    
